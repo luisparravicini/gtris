a3ab56f5a4546c598344488e9dcd0364a4d0ad33
Luis Parravicini <lparravi@gmail.com>
2022-06-15 17:20:24 -0300

    ebiten example used as draft

---
+++ main.go
@@ -0,0 +1,68 @@
+package gtris
+
+import (
+	"bytes"
+	"image"
+	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+var (
+	ebitenImage *ebiten.Image
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Nearest Filter (default) VS Linear Filter")
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(4, 4)
+	op.GeoM.Translate(64, 64)
+	// By default, nearest filter is used.
+	screen.DrawImage(ebitenImage, op)
+
+	op = &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(4, 4)
+	op.GeoM.Translate(64, 64+240)
+	// Specify linear filter.
+	op.Filter = ebiten.FilterLinear
+	screen.DrawImage(ebitenImage, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	// Decode an image from the image file's byte slice.
+	// Now the byte slice is generated with //go:generate for Go 1.15 or older.
+	// If you use Go 1.16 or newer, it is strongly recommended to use //go:embed to embed the image file.
+	// See https://pkg.go.dev/embed for more details.
+	img, _, err := image.Decode(bytes.NewReader(images.Ebiten_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ebitenImage = ebiten.NewImageFromImage(img)
+
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Filter (Ebiten Demo)")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
