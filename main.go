2c52c2f0e62485634c73ef3b0952e2af06cd1d63
Luis Parravicini <lparravi@gmail.com>
2022-06-15 18:02:06 -0300

    Drawing the whole board

--- main.go
+++ main.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"image"
 	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 )
 
 const (
-	screenWidth  = 640
-	screenHeight = 480
+	screenWidth  = 256
+	screenHeight = 240
 )
 
-var (
-	ebitenImage *ebiten.Image
-)
+//go:embed images/block.png
+var imgBlock []byte
 
 type Game struct {
+	blockImage *ebiten.Image
 }
 
 func (g *Game) Update() error {
@@ -30,18 +30,15 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Nearest Filter (default) VS Linear Filter")
 
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(4, 4)
-	op.GeoM.Translate(64, 64)
-	// By default, nearest filter is used.
-	screen.DrawImage(ebitenImage, op)
-
-	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(4, 4)
-	op.GeoM.Translate(64, 64+240)
-	// Specify linear filter.
-	op.Filter = ebiten.FilterLinear
-	screen.DrawImage(ebitenImage, op)
+	w, h := g.blockImage.Size()
+	for x := 0; x < 10; x++ {
+		for y := 0; y < 24; y++ {
+			op := &ebiten.DrawImageOptions{}
+			// op.GeoM.Scale(4, 4)
+			op.GeoM.Translate(float64(x*w), float64(y*h))
+			screen.DrawImage(g.blockImage, op)
+		}
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -49,20 +46,18 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	// Decode an image from the image file's byte slice.
-	// Now the byte slice is generated with //go:generate for Go 1.15 or older.
-	// If you use Go 1.16 or newer, it is strongly recommended to use //go:embed to embed the image file.
-	// See https://pkg.go.dev/embed for more details.
-	img, _, err := image.Decode(bytes.NewReader(images.Ebiten_png))
+	img, _, err := image.Decode(bytes.NewReader(imgBlock))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ebitenImage = ebiten.NewImageFromImage(img)
+	game := &Game{
+		blockImage: ebiten.NewImageFromImage(img),
+	}
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Filter (Ebiten Demo)")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
